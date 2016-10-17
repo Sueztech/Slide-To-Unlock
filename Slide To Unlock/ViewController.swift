35acffb77ac6f34fb8e199e26d46fe71a1fb7721
Kevin Abraham <kev@gatech.edu>
2016-10-17 12:05:05 -0400

    Initial Commit

---
+++ Slide To Unlock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Slide To Unlock
+//
+//  Created by Kevin Abraham on 10/17/16.
+//  Copyright © 2016 Sueztech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Slide To UnlockTests/Slide_To_UnlockTests.swift
@@ -0,0 +1,36 @@
+//
+//  Slide_To_UnlockTests.swift
+//  Slide To UnlockTests
+//
+//  Created by Kevin Abraham on 10/17/16.
+//  Copyright © 2016 Sueztech. All rights reserved.
+//
+
+import XCTest
+@testable import Slide_To_Unlock
+
+class Slide_To_UnlockTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
